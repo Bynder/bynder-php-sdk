0c9cedcddfbf1d10a9cf24e6aaa82d9314e1403e
Dave Reid <68073680+davereid-pfg@users.noreply.github.com>
2022-11-09 12:00:16 -0600

    Fixed namespace of Exception in AbstractRequestHandler

--- src/Bynder/Api/Impl/AbstractRequestHandler.php
+++ src/Bynder/Api/Impl/AbstractRequestHandler.php
@@ -12,7 +12,7 @@ abstract class AbstractRequestHandler
         );
 
         if (!in_array($requestMethod, ['GET', 'POST', 'DELETE'])) {
-            throw new Exception('Invalid request method provided');
+            throw new \Exception('Invalid request method provided');
         }
 
         $request = $this->sendAuthenticatedRequest($requestMethod, $uri, $options);
@@ -28,7 +28,7 @@ abstract class AbstractRequestHandler
                     case 'text/html':
                         return $response;
                     default:
-                        throw new Exception('The response type not recognized.');
+                        throw new \Exception('The response type not recognized.');
                 }
             }
         );
