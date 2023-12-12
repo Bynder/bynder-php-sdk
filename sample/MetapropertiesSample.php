d84a0a1860223bd5b4739edd753fd7e03dadb35b
ahongbynder <alex.hong@bynder.com>
2023-12-12 15:24:06 -0800

    API-1824 metaproperty changes, options/dependencies for sample file

--- sample/MetapropertiesSample.php
+++ sample/MetapropertiesSample.php
@@ -50,8 +50,61 @@ try {
             var_dump($metaproperty);
         }
     }
-    var_dump($metapropertiesList);
 
+    // get info for specific metaproperty
+    $metapropertyInfoPromise = $assetBankManager->getMetaproperty($METAPROPERTY_ID_FOR_INFO);
+    $metapropertyInfo = $metapropertyInfoPromise->wait();
+
+    if (!empty($metapropertyInfo)) {
+        echo("Metaproperty Info for ID: " . $METAPROPERTY_ID_FOR_INFO);
+        var_dump($metapropertyInfo);
+    }
+
+    // get metaproperty dependencies
+    $metapropertyDependenciesPromises = $assetBankManager->getMetapropertyDependencies($METAPROPERTY_ID_FOR_DEPENDENCY_INFO);
+    $metapropertyDependencies = $metapropertyDependenciesPromises->wait();
+
+    if (!empty($metapropertyDependencies)) {
+        echo("Metaproperty Dependencies for ID: " . $METAPROPERTY_ID_FOR_DEPENDENCY_INFO);
+        var_dump($metapropertyDependencies);
+    }
+
+    // get metaproperty option ids
+    $query = ["ids" => $METAPROPERTY_OPTION_ID_FOR_INFO];
+    $metapropertyOptionsPromise = $assetBankManager->getMetapropertyOptions($query);
+    $metapropertyOptions = $metapropertyOptionsPromise->wait();
+
+    if (!empty($metapropertyOptions)) {
+        echo("Metaproperty Options for ID: " . $METAPROPERTY_OPTION_ID_FOR_INFO);
+        var_dump($metapropertyOptions);
+    }
+
+    // get metaproperty global option dependencies
+    $metapropertyGlobalOptionDependenciesPromise = $assetBankManager->getMetapropetryGlobalOptionDependencies();
+    $metapropertyGlobalOptionDependencies = $metapropertyGlobalOptionDependenciesPromise->wait();
+
+    if (!empty($metapropertyGlobalOptionDependencies)) {
+        echo("Metaproperty Global Option Dependencies" . "\n");
+        var_dump($metapropertyGlobalOptionDependencies);
+    }
+
+    // get metaproperty option dependencies
+    $metapropertyOptionDependenciesPromise = $assetBankManager->getMetapropertyOptionDependencies($METAPROPERTY_ID_FOR_DEPENDENCY_INFO);
+    $metapropertyOptionsDependencies = $metapropertyOptionDependenciesPromise->wait();
+
+    if (!empty($metapropertyOptionsDependencies)) {
+        echo("Metaproperty Options Dependencies for ID: " . $METAPROPERTY_ID_FOR_DEPENDENCY_INFO . "\n");
+        var_dump($metapropertyOptionsDependencies);
+    }
+
+    // get metaproperty specific option dependencies
+    $metapropertySpecificOptionDependenciesPromise = $assetBankManager->getMetapropertySpecificOptionDependencies($METAPROPERTY_ID_FOR_SPECIFIC_OPTION_DEPEND, $METAPROPERTY_OPTION_ID_FOR_SPECIFIC_OPTION_DEPEND, ['includeGroupedResults' => false]);
+    $metapropertySpecificOptionDependencies = $metapropertySpecificOptionDependenciesPromise->wait();
+
+    if (!empty($metapropertySpecificOptionDependencies)) {
+        echo("Metaproperty Specific Option Dependencies" . "\n");
+        var_dump($metapropertySpecificOptionDependencies);
+    }
 } catch (Exception $e) {
     var_dump($e);
 }
