a0e3a63ba74dd70b2ba414b661678d7c932c121a
constant-bouquet <constant.bouquet@gmail.com>
2024-01-22 18:43:07 +0100

    Add new API endpoints - createMetaPropertyOption - modifyMetaPropertyOption - syncAssetUsage - getAccount

--- src/Bynder/Api/Impl/AssetBankManager.php
+++ src/Bynder/Api/Impl/AssetBankManager.php
@@ -60,6 +60,17 @@ class AssetBankManager
     }
 
     /**
+     * Gets account information
+     *
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     * @throws \Exception
+     */
+    public function getAccount()
+    {
+        return $this->requestHandler->sendRequestAsync('GET', "api/v4/account/");
+    }
+
+    /**
      * Gets a list of all available brands.
      *
      * @return \GuzzleHttp\Promise\Promise with a list of all Brands.
@@ -168,6 +179,42 @@ class AssetBankManager
     {
         return $this->requestHandler->sendRequestAsync('GET', 'api/v4/metaproperties/' . $propertyId . '/options/',
             ['query' => $query]
+        );
+    }
+
+    /**
+     * Creates an option for a metaproperty.
+     *
+     * @param string $metaPropId
+     * @param array $data
+     * @return \GuzzleHttp\Promise\Promise
+     * @throws \Exception
+     */
+    public function createMetaPropertyOption($metaPropId, $data)
+    {
+        return $this->requestHandler->sendRequestAsync(
+            'POST',
+            sprintf('api/v4/metaproperties/%s/options/', $metaPropId),
+            ['form_params' => ['data' => json_encode($data)]]
+        );
+    }
+
+    /**
+     * Modifies existing metaproperty option.
+     *
+     * @param string $metaPropId
+     * @param string $optionId
+     * @param array  $data
+     *
+     * @return \GuzzleHttp\Promise\Promise
+     * @throws \Exception
+     */
+    public function modifyMetaPropertyOption($metaPropId, $optionId, $data)
+    {
+        return $this->requestHandler->sendRequestAsync(
+            'POST',
+            sprintf('api/v4/metaproperties/%s/options/%s/', $metaPropId, $optionId),
+            ['form_params' => ['data' => json_encode($data)]]
         );
     }
 
@@ -386,12 +433,27 @@ class AssetBankManager
      *
      * @param  $query
      * @return \GuzzleHttp\Promise\Promise
-     * @throws \GuzzleHttp\Exception\RequestException 
+     * @throws \GuzzleHttp\Exception\RequestException
      */
     public function deleteUsage($query)
     {
         return $this->requestHandler->sendRequestAsync('DELETE', 'api/media/usage',
             ['query' => $query]
+        );
+    }
+
+    /**
+     * Synchronizes asset usages
+     *
+     * @param array $data
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function syncAssetUsage($data)
+    {
+        return $this->requestHandler->sendRequestAsync('POST', 'api/media/usage/sync',
+            ['json' => $data]
         );
     }
 
