d7447c0766fc7e4636dfadadc311c105870c9684
constant-bouquet <constant.bouquet@gmail.com>
2025-01-31 19:22:08 +0100

    Do not override request options User-Agent

--- src/Bynder/Api/Impl/PermanentTokens/RequestHandler.php
+++ src/Bynder/Api/Impl/PermanentTokens/RequestHandler.php
@@ -14,7 +14,7 @@ class RequestHandler extends AbstractRequestHandler
     public function __construct($configuration)
     {
         $this->configuration = $configuration;
-        $this->httpClient = new \GuzzleHttp\Client();
+        $this->httpClient    = new \GuzzleHttp\Client();
     }
 
     protected function sendAuthenticatedRequest($requestMethod, $uri, $options = [])
@@ -25,16 +25,20 @@ class RequestHandler extends AbstractRequestHandler
 
         $request = new \GuzzleHttp\Psr7\Request($requestMethod, $uri, $headers);
 
-        return $this->httpClient->sendAsync(
-            $request,
-            array_merge(
-                $options,
-                $this->configuration->getRequestOptions(),
-                ['headers'=> [
-                    'User-Agent' => 'bynder-php-sdk/' . $this->configuration->getSdkVersion(),
-                    'Authorization' => 'Bearer ' . $this->configuration->getToken()
-                ]]
-            )
+        $requestOptions = array_merge(
+            $options,
+            $this->configuration->getRequestOptions(),
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->configuration->getToken(),
+                ],
+            ]
         );
+
+        if (!isset($requestOptions['headers']['User-Agent'])) {
+            $requestOptions['headers']['User-Agent'] = 'bynder-php-sdk/' . $this->configuration->getSdkVersion();
+        }
+
+        return $this->httpClient->sendAsync($request, $requestOptions);
     }
 }
